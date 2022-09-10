b3310c1ff7d1464a79603f9ab7e392b90fd6ce0a
Marco <marcocarrilloosuna@gmail.com>
2022-09-10 17:58:12 -0500

    :art: :rocket: Add: Rutas
    Configuracion de navegacion de rutas para que a largo plazo sea mas mantenible

---
+++ src/layouts/ClientLayout/ClientLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ClientLayout.scss';
+
+export function ClientLayout(props) {
+    const { children } = props;
+    return (
+        <div>
+            <h1>ClientLayout</h1>
+            {children}
+        </div> 
+    );
+}

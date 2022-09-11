3a693fc0123eea2333e7bddad51a4ebae90d6dd2
Marco <marcocarrilloosuna@gmail.com>
2022-09-11 00:26:14 -0500

    :sparkles: add: validacion del form de login
    Se agrego la validacion del login del dashboard de administracion usando formik y yup.

--- src/components/Admin/LoginForm/LoginForm.js
+++ src/components/Admin/LoginForm/LoginForm.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import { Button, Form, Image, Header } from 'semantic-ui-react';
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import './LoginForm.scss';
 
 
 export function LoginForm() {
-  return (
-    <Form className='login-form-admin' >
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(validationSchema()),
+        onSubmit: (formValue) => {
+            console.log(formValue);
+        }
+    });
+
+    return (
+    <Form className='login-form-admin' onSubmit={formik.handleSubmit}>
         <Image src='https://i.ibb.co/0B3MdZx/logo-tiburoneros.jpg' size='small' centered />
         <Header as='h2'>Administración</Header>
         <Form.Input 
             name='email'
             placeholder='Correo electrónico'
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.errors.email}
         />
         <Form.Input 
             name='password'
             placeholder='Contraseña'
             type='password'
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.errors.password}
         />
         <Button type='submit' content='Iniciar Sesión' primary fluid />
     </Form>
-  )
+    );
+}
+
+function initialValues() {
+    return {
+        email: "",
+        password: ""
+    }
+}
+
+function validationSchema() {
+    return {
+        email: Yup.string().email(true).required(true),
+        password: Yup.string().required(true)
+    };
 }

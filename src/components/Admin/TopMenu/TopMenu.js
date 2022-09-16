69cd82ef2b7ee2adf185fe3ced514b8bc2a68de7
Marco <marcocarrilloosuna@gmail.com>
2022-09-16 16:53:59 -0500

    :sparkles: feat: estructura y diseño del TopMenu
    Se añadio el TabMenu para poder cerrar sesion y tener una estructura visual del sitio mas ordenada

---
+++ src/components/Admin/TopMenu/TopMenu.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Icon, Menu, Image } from 'semantic-ui-react';
+import { useAuth }  from '../../../hooks'
+import { useUser } from '../../../hooks';
+import './TopMenu.scss';
+import { TOKEN } from '../../../utils/constants';
+
+export function TopMenu() {
+    const { auth, logout } = useAuth();
+
+
+    const renderName = () => {
+        if(auth === null || auth === undefined) {
+            return 'Bienvenido!';
+        }
+        if(auth.me?.first_name && auth.me?.last_name) {
+            return `Hola, Bienvenido ${auth.me.first_name} ${auth.me.last_name}!`;
+        }
+        return auth.me?.email;
+    }
+
+  return (
+    <Menu fixed='top' className='top-menu-admin'>
+        <Menu.Item className='top-menu-admin__logo'>
+            <Image src='https://i.ibb.co/0B3MdZx/logo-tiburoneros.jpg' size='tiny' centered />
+        </Menu.Item>
+        <Menu.Menu position='right'>
+            <Menu.Item>{renderName()}</Menu.Item>
+            <Menu.Item onClick={logout}>
+                <Icon size='large' name="sign out" />
+                <p>Cerrar Sesion</p>
+            </Menu.Item>
+        </Menu.Menu>
+    </Menu>
+  )
+}
--- src/layouts/AdminLayout/AdminLayout.js
+++ src/layouts/AdminLayout/AdminLayout.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './AdminLayout.scss';
-import { LoginAdmin } from  '../../pages/Admin'
+import { LoginAdmin } from  '../../pages/Admin';
+import { TopMenu } from '../../components/Admin';
 import { useAuth } from '../../hooks';
 import { TOKEN } from '../../utils/constants';
 
 export function AdminLayout(props) {
     const { children } = props;
     const { auth } = useAuth();
-    console.log(auth)
-
     if(!localStorage.getItem(TOKEN)) return <LoginAdmin />;
 
     return (
-        <div>
-            <h2>AdminLayout</h2>
-            {children}
+        <div className='admin-layout'>
+            <div className='admin-layout__menu'>
+                <TopMenu />
+            </div>
+            <div className='admin-layout__main-content'>
+                {children}
+            </div>
         </div>
     );
 }

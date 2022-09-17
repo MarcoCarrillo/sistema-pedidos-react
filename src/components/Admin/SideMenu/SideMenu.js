cbdbe68ef54fc55c65485f7b89ed1d53a1d79479
Marco <marcocarrilloosuna@gmail.com>
2022-09-16 21:10:30 -0500

    :sparkles: feat: Restringir acceso segun el rol
    Se restringe el acceso a algunos modulos a usuarios que no pertenecen al staff de administradores

--- src/components/Admin/SideMenu/SideMenu.js
+++ src/components/Admin/SideMenu/SideMenu.js
@@ -17,7 +17,8 @@ export function SideMenu(props) {
 
 function MenuLeft(props) {
     const { pathname } = props;
-
+    const { auth } = useAuth();
+    console.log(auth);
 
     return(
         <Menu fixed='left' borderless className='side' vertical>
@@ -27,18 +28,24 @@ function MenuLeft(props) {
             <Menu.Item as={Link} to={'/admin/tables'} active={pathname === '/admin/tables'}>
                 <p className='item-menu'><Icon name='microchip' size='large' /> Mesas</p>
             </Menu.Item>
-            <Menu.Item as={Link} to={'/admin/payments-history'} active={pathname === '/admin/payments-history'}>
-                <p className='item-menu'><Icon name='history' size='large' /> Historial de Pedidos</p>
-            </Menu.Item>
-            <Menu.Item as={Link} to={'/admin/categories'} active={pathname === '/admin/categories'}>
-                <p className='item-menu'><Icon name='book' size='large' /> Categorias</p>
-            </Menu.Item>
-            <Menu.Item as={Link} to={'/admin/products'} active={pathname === '/admin/products'}>
-                <p className='item-menu'><Icon name='food' size='large' /> Productos</p>
-            </Menu.Item>
-            <Menu.Item as={Link} to={'/admin/users'} active={pathname === '/admin/users'}>
-                <p className='item-menu'><Icon name='users' size='large' /> Usuarios</p>
-            </Menu.Item>
+            {auth.me?.is_staff && (
+                <div>
+                    <Menu.Item as={Link} to={'/admin/payments-history'} active={pathname === '/admin/payments-history'}>
+                        <p className='item-menu'><Icon name='history' size='large' /> Historial de Pedidos</p>
+                    </Menu.Item>
+                    <Menu.Item as={Link} to={'/admin/categories'} active={pathname === '/admin/categories'}>
+                        <p className='item-menu'><Icon name='book' size='large' /> Categorias</p>
+                    </Menu.Item>
+                    <Menu.Item as={Link} to={'/admin/products'} active={pathname === '/admin/products'}>
+                        <p className='item-menu'><Icon name='food' size='large' /> Productos</p>
+                    </Menu.Item>
+                    <Menu.Item as={Link} to={'/admin/users'} active={pathname === '/admin/users'}>
+                        <p className='item-menu'><Icon name='users' size='large' /> Usuarios</p>
+                    </Menu.Item>
+                </div>
+            
+            )}
+            
         </Menu>
     );
 }

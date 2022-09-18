62bd2690dd74db08b2ff02bcb4c9d4107ebaafb1
Marco <marcocarrilloosuna@gmail.com>
2022-09-18 14:54:34 -0500

    :sparkles: feat: Modal
    Creacion de un modal reutilizable para los diferentes botones del sistema

--- src/pages/Admin/UsersAdmin.js
+++ src/pages/Admin/UsersAdmin.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { HeaderPage, TableUsers } from '../../components/Admin';
 import { Loader } from 'semantic-ui-react';
+import { ModalBasic } from '../../components/Common';
 import { useUser } from '../../hooks';
 
 export function UsersAdmin() {
@@ -8,14 +9,19 @@ export function UsersAdmin() {
   console.log('loading',loading);
   console.log('users' ,users);
 
+  const [titleModal, setTitleModal] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [contentModal, setContentModal] = useState(null);
+
   useEffect(() => {
     getUsers();
   }, [])
   
+  const openCloseModal = () => setShowModal( prev => !prev);
 
   return (
     <>
-      <HeaderPage title='Usuarios' btnTitle='Nuevo usuario'/>
+      <HeaderPage title='Usuarios' btnTitle='Nuevo usuario' btnClick={openCloseModal} />
       {loading ? (
         <Loader active inline='centered'>
           Cargando...
@@ -23,6 +29,8 @@ export function UsersAdmin() {
       ) : (
         <TableUsers users={users}/>
       )}
+
+      <ModalBasic show={showModal} onClose={openCloseModal} title='Crear Usuario' children={<h2>Content</h2>} />
     </>
   )
 }

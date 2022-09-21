a69f88ddabf9c022d6af45ee06d7e4d7fc83b4be
Marco <marcocarrilloosuna@gmail.com>
2022-09-20 22:23:25 -0500

    :sparkles: feat: header, boton y ruta de categorias

---
+++ src/pages/Admin/CategoriasAdmin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HeaderPage } from '../../components/Admin'
+
+export function CategoriasAdmin() {
+  return (
+    <>
+        <HeaderPage
+            title= "Categorias" btnTitle="Nueva categoria"
+        />    
+    </>
+  )
+}

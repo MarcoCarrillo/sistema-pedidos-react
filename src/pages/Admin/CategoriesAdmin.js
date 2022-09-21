4ae0a089934f6e1ce22e4b1573aa784bf86ca726
Marco <marcocarrilloosuna@gmail.com>
2022-09-21 00:09:00 -0500

    :sparkles: feat: form para crear/actualizar categoria
    Se realizo el formulario modal para añadir o editar categorias usando un dropzone para las imagenes y controlando el state

--- src/pages/Admin/CategoriesAdmin.js
+++ src/pages/Admin/CategoriesAdmin.js
@@ -1,22 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Loader } from 'semantic-ui-react';
-import { HeaderPage, TableCategoryAdmin } from '../../components/Admin';
+import { AddEditCategoryForm, HeaderPage, TableCategoryAdmin } from '../../components/Admin';
 import { useCategory } from '../../hooks';
+import { ModalBasic } from '../../components/Common';
 
 export function CategoriasAdmin() {
+    const [showModal, setShowModal] = useState(false)
+    const [titleModal, setTitleModal] = useState(null)
+    const [contentModal, setContentModal] = useState(null)
     const { loading, categories, getCategories } = useCategory();
     useEffect(() => {
         getCategories();
-    }, [])
+    }, []);
+
+    const openCloseModal = () => setShowModal(prev => !prev);
+
+    const addCategory = () => {
+        setTitleModal('Nueva categoria');
+        setContentModal(<AddEditCategoryForm/>)
+        openCloseModal()
+    }
     
   return (
     <>
         <HeaderPage
-            title= "Categorias" btnTitle="Nueva categoria"
+            title= "Categorias" btnTitle="Nueva categoria" btnClick={addCategory}
         />    
         {loading ? (
             <Loader active inline="centered">Cargando...</Loader>
         ): <TableCategoryAdmin categories={categories} />}
+        <ModalBasic 
+            show={showModal}
+            onClose={openCloseModal}
+            title={titleModal}
+            children={contentModal}
+        />
     </>
   )
 }

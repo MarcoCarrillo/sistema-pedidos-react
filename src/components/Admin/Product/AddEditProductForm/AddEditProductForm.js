8d8e08b0f2f877e373adccb27c0bdb5082513653
Marco <marcocarrilloosuna@gmail.com>
2022-10-09 17:43:47 -0500

    :rocket: feat: state y validacion en form de productos
    Se validan los campos y se capturan los valores del formulario

--- src/components/Admin/Product/AddEditProductForm/AddEditProductForm.js
+++ src/components/Admin/Product/AddEditProductForm/AddEditProductForm.js
@@ -4,7 +4,9 @@ import { Form, Image, Button, Dropdown, Checkbox } from 'semantic-ui-react';
 import { useCategory } from '../../../../hooks';
 import { useDropzone } from 'react-dropzone';
 import { map } from 'lodash';
-import './AddEditProductForm.scss'
+import { useFormik } from 'formik';
+import * as Yup from 'yup'; 
+import './AddEditProductForm.scss';
 
 export function AddEditProductForm() {
     const [categoriesFormat, setCategoriesFormat] = useState([]);
@@ -12,12 +14,25 @@ export function AddEditProductForm() {
     const { categories, getCategories } = useCategory();
 
     useEffect(() => {
-        getCategories();
+        getCategories()
+    }, []);
+    useEffect(() => {
         setCategoriesFormat(formatDropdownData(categories))
-    }, [categories]);
+    }, [categories])
+
+    const formik = useFormik({
+        initialValues: initialValues,
+        validationSchema: Yup.object(newSchema()),
+        validateOnChange: false,
+        onSubmit: (formValue) => {
+            console.log('Form enviado');
+            console.log(formValue);
+        }
+    })
 
     const onDrop = useCallback(async (acceptedFile) => {
         const file = acceptedFile[0];
+        await formik.setFieldValue('image', file);
         setPreviewImage(URL.createObjectURL(file));
     }, [])
 
@@ -29,21 +44,51 @@ export function AddEditProductForm() {
     })
 
   return (
-    <Form className='add-edit-product-form'>
-        <Form.Input name='title' placeholder='Nombre del producto' />
-        <Form.Input name='price' type='number' placeholder='Precio' />
+    <Form className='add-edit-product-form' onSubmit={formik.handleSubmit}>
+        <Form.Input 
+            name='title' 
+            placeholder='Nombre del producto' 
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            error={formik.errors.title} 
+        />
+        <Form.Input 
+            name='price' 
+            type='number' 
+            placeholder='Precio' 
+            value={formik.values.price}
+            onChange={formik.handleChange}
+            error={formik.errors.title} 
+        />
         
-        <Dropdown placeholder='Categoria' fluid selection search options={categoriesFormat} />
+        <Dropdown 
+            placeholder='Categoria' 
+            fluid 
+            selection 
+            search 
+            options={categoriesFormat} 
+            value={formik.values.category}
+            error={formik.errors.category}
+            onChange={(_, data) => formik.setFieldValue('category', data.value)}   
+        />
 
         <div className='add-edit-product-form__active'>
-            <Checkbox toggle />
+            <Checkbox 
+                toggle 
+                checked={formik.values.active}
+                onChange={(_, data) => formik.setFieldValue('active', data.checked)}
+            />
             Producto activo
         </div>
         
+        <Button 
+            type='button' 
+            fluid {...getRootProps()} color={formik.errors.image && 'red'}>
+            {previewImage ? 'Cambiar imagen' : 'Subir imagen'}
+        </Button>
         <input {...getInputProps()} />
-        
-        <Button type='button' fluid {...getRootProps()}>Subir imagen</Button>
         <Image src={previewImage} />
+
         <Button type='submit' primary fluid>Crear producto</Button>
     </Form>
   )
@@ -55,4 +100,24 @@ function formatDropdownData(data) {
         text: item.title,
         value: item.id
     }))
+}
+
+function initialValues() {
+    return {
+        title: '',
+        price: '',
+        category: '',
+        active: false,
+        image: ''
+    }
+}
+
+function newSchema() {
+    return {
+        title: Yup.string().required(true),
+        price: Yup.number().required(true),
+        category: Yup.number().required(true),
+        active: Yup.boolean().required(true),
+        image: Yup.string().required(true)
+    }
 }

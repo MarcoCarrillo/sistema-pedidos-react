e5e3c89b1a6338a58ed5da1c958fafc2a3c12849
Marco <marcocarrilloosuna@gmail.com>
2022-10-12 22:42:45 -0500

    :construction: feat: formulario, validacion y estado al crear mesas

--- src/pages/Admin/TablesAdmin.js
+++ src/pages/Admin/TablesAdmin.js
@@ -1,25 +1,37 @@
-import React, {Fragment, useEffect} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import { Loader } from 'semantic-ui-react';
-import { HeaderPage, TableTablesAdmin } from '../../components/Admin';
+import { HeaderPage, TableTablesAdmin, AddEditTableForm } from '../../components/Admin';
+import { ModalBasic } from '../../components/Common';
 import { useTable } from '../../hooks';
 
 export function TablesAdmin() {
+    const [showModal, setShowModal] = useState(false);
+    const [titleModal, setTitleModal] = useState(null);
+    const [contentModal, setContentModal] = useState(null);
     const { loading, tables, getTables } = useTable();
 
     useEffect(() => {
       getTables()
     }, []);
 
-    console.log(tables);
+    const openCloseModal = () => setShowModal(prev => !prev);
+
+    const addTable = () => {
+        setTitleModal('Crear mesa');
+        setContentModal(<AddEditTableForm onClose={openCloseModal} />);
+        openCloseModal();
+    }
     
     return (
         <Fragment>
-            <HeaderPage title='Mesas' btnTitle='Crear nueva mesa' />
+            <HeaderPage title='Mesas' btnTitle='Crear nueva mesa'  btnClick={addTable}/>
             {loading ? 
                 <Loader active inline='centered'>Cargando...</Loader>
             :(
                 <TableTablesAdmin tables={tables} />
             )}
+
+            <ModalBasic show={showModal} onClose={openCloseModal} title={titleModal} children={contentModal} />
         </Fragment>
     )
 }

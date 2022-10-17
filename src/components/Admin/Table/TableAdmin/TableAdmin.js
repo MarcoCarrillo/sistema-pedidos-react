b3d0236926849a3be72e986059be40238a29aa85
Marco <marcocarrilloosuna@gmail.com>
2022-10-16 21:15:05 -0500

    :rocket: feat: Pedidos por mesa
    Obtener los pedidos pendientes de cada mesa e identificar las mesas con pedidos pendientes

--- src/components/Admin/Table/TableAdmin/TableAdmin.js
+++ src/components/Admin/Table/TableAdmin/TableAdmin.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { size } from 'lodash';
+import { Label } from 'semantic-ui-react';
+import classNames from 'classnames';
 import { ReactComponent as IcTable } from '../../../../assets/table.svg';
+import { getOrdersByTableApi } from '../../../../api/orders';
+import { ORDER_STATUS } from '../../../../utils/constants';
 import './TableAdmin.scss'
+
 
 export function TableAdmin(props) {
   const { table } = props;
+  const [orders, setOrders] = useState([]);
+  console.log(orders);
 
+  useEffect(() => {
+    (async () => {
+      const response = await getOrdersByTableApi(table.number, ORDER_STATUS.PENDING);
+      setOrders(response);
+    })()
+  }, [])
   return (
     <div className='table-admin'>
-      <IcTable />
+      {size(orders) > 0 ? (
+        <Label circular color='orange' >{size(orders)}</Label>
+      ) : null}
+      <IcTable className={classNames({
+        pending: size(orders) > 0,
+      })} />
       <p>Mesa {table.number}</p>
     </div>
   )

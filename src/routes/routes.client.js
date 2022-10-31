eba944c9308a34e7c416d0b5fb161c219f516810
Marco <marcocarrilloosuna@gmail.com>
2022-10-30 19:36:21 -0600

    :construction: feat: ruta y screen de categorias

--- src/routes/routes.client.js
+++ src/routes/routes.client.js
@@ -1,12 +1,17 @@
-import { ClientLayout, BasicLayout } from "../layouts";
-import { SelectTable } from "../pages/Client";
-import { Error404 } from "../pages";
+import { BasicLayout, ClientLayout } from "../layouts";
+import { SelectTable, Categories } from "../pages/Client";
 
 const routesClient = [
     {
         path: "/",
         layout: BasicLayout,
         component: SelectTable,
+        exact: true
+    },
+    {
+        path: "/client/:tableNumber",
+        layout: ClientLayout,
+        component: Categories,
         exact: true
     }
 ];

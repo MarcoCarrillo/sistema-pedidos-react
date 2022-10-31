9784bd51c58ae3fcd916d3825e3f1c648b98f314
Marco <marcocarrilloosuna@gmail.com>
2022-10-30 19:56:55 -0600

    :rocket: feat: layout del cliente

--- src/layouts/ClientLayout/ClientLayout.js
+++ src/layouts/ClientLayout/ClientLayout.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { Container, Icon, Button, Image } from 'semantic-ui-react';
 import { useTable } from '../../hooks';
 import './ClientLayout.scss';
 
@@ -21,13 +22,40 @@ export function ClientLayout(props) {
     }, [tableNumber]);
 
     const closeTable = () => {
+        navigate('/');
+    }
 
+    const goToCart = () => {
+        navigate(`/client/${tableNumber}/cart`);
+    }
+
+    const goToOrders = () => {
+        navigate(`/client/${tableNumber}/orders`);
     }
 
     return (
-        <div>
-            <h1>ClientLayout</h1>
-            {children}
-        </div> 
+        <div className="client-layout-bg">
+      <Container className="client-layout">
+        <div className="client-layout__header">
+          <Link to={`/client/${tableNumber}`}>
+            <Image src='https://i.ibb.co/0B3MdZx/logo-tiburoneros.jpg' size='tiny' centered />
+          </Link>
+          <span>Mesa {tableNumber}</span>
+
+          <div>
+            <Button icon onClick={goToCart}>
+              <Icon name="shop" />
+            </Button>
+            <Button icon onClick={goToOrders}>
+              <Icon name="list" />
+            </Button>
+            <Button color='red' icon onClick={closeTable}>
+              <Icon  name="sign out" />
+            </Button>
+          </div>
+        </div>
+        <div className="client-layout__content">{children}</div>
+      </Container>
+    </div>
     );
 }

bd3e6773dbfb022f28366951136055354c130a1f
Marco <marcocarrilloosuna@gmail.com>
2022-10-30 19:46:23 -0600

    :lock: feat: seguridad al modificar la url
    Si el usuario modifica el url con un numero de mesa que no existe validarlo

--- src/layouts/ClientLayout/ClientLayout.js
+++ src/layouts/ClientLayout/ClientLayout.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useTable } from '../../hooks';
 import './ClientLayout.scss';
 
 export function ClientLayout(props) {
     const { children } = props;
+    const { isExistTable } = useTable();
+    const { tableNumber } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        (async () => {
+            const exist = await isExistTable(tableNumber);
+            if(!exist) {
+                navigate('/');
+                toast.error(`La mesa ${tableNumber} no existe`);
+            } 
+        })()
+    }, [tableNumber]);
+
+    const closeTable = () => {
+
+    }
+
     return (
         <div>
             <h1>ClientLayout</h1>

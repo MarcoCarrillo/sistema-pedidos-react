7e4521befd8f9eaae2e712d8d03f9a2dd44bd120
Marco <marcocarrilloosuna@gmail.com>
2022-11-05 00:22:07 -0600

    :bug: fix: page de error 404

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Error404.css';
 
 export function Error404() {
   return (
-    <h1>Error 404</h1>
+    <body className='e404'>
+    <section className="notFound error404">
+        <div className="img">
+        <img src="https://assets.codepen.io/5647096/backToTheHomepage.png" alt="Back to the Homepage"/>
+        <img src="https://assets.codepen.io/5647096/Delorean.png" alt="El Delorean, El Doc y Marti McFly"/>
+        </div>
+        <div className="text404">
+        <h1>404</h1>
+        <h2>PAGE NOT FOUND</h2>
+        <h3>BACK TO HOME?</h3>
+        <a href="https://sistema-pedidos-tiburoneros.netlify.app/" class="yes">YES</a>
+        <a href="https://www.youtube.com/watch?v=G3AfIvJBcGo">NO</a>
+        </div>
+    </section>
+</body>
   )
 }
